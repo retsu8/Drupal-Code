7dff51eef174f548f068c5016bf9e48fc3c3e82a
(retsu8)Wiliam Paddock <wjp@frontline.local>
2016-07-15 16:25:15 -0600

    quick clean
    done

--- ocdlogin.php
+++ ocdlogin.php
@@ -3,4 +3,4 @@ $nid= arg(1);
 $node = node_load($nid);
 $user_mid = $node->field_portfolio_manager_username['und'][0]['value'];
 ?>
-<a href="http://54.186.135.80:8000/mid/<?php echo $user_mid ?>" target="_blank">Click for graph</a>
+<a href="http://54.186.135.80/mid/<?php echo $user_mid ?>" target="_blank">Click for graph</a>
