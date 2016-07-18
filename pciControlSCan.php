b93bdc464f06a15f5f50c2d9aee66c2f9c76b3b6
(retsu8)Wiliam Paddock <wjp@frontline.local>
2016-07-18 13:30:38 -0600

    working cookie
    working cookie to set userid

--- pciControlSCan.php
+++ pciControlSCan.php
@@ -1,12 +1,19 @@
+<script type = "text/javascript">
+         <!--
+            function WriteCookie(){
+							if( document.admlogin.password.value == "" ){
+								 alert("No password entered!!");
+								 return;
+							 }
+							//else if(document.admlogin.remember_me.checked == true){
+								document.cookie = "userid=" +document.admlogin.password.value;
+						 //}
+					}
+         //-->
+      </script>
 <?php
 $nid= arg(1);
 $node = node_load($nid);
-if(isset($_COOKIE["userid"])){
-	print "Cookie Set";
-}
-else{
-	print "No Cookie";
-}
 ?>
 
 
@@ -17,22 +24,16 @@ else{
 	<label>Username:</label><input class="ssaq_form" type="text" size="42"  style="background-color: lightgray; width: 180px;" readonly="" name="username" value = "<?php echo $node->field_pci_controlscan_username['und'][0]['value'];   ?>">
 	<br />
 <?php
-if(!isset($_COOKIE[$userid])) {
+if(!isset($_COOKIE["userid"])) {
     echo '<label>Password:</label>';
 		?><input class="ssaq_form" type="password" size="42" name="password" style = "width: 180px;" >
 <?php } else {
 	echo '<label>Password:</label>'; ?>
- <input class="ssaq_form" type="password" size="42" name="password" style = "width: 180px;" value = "<?php echo $_COOKIE[$userid]?>">	<?php
+ <input class="ssaq_form" type="password" size="42" name="password" style = "width: 180px;" value = "<?php echo $_COOKIE["userid"]?>">	<?php
 }
 ?><br>
 		<label class="option" for="remember_me">Remember me: </label><input class="ssaq_form" type="checkbox" name="remember_me" value="1" />
 	<div align="left">
-		<input class="button ssaq_button" type="Submit" value="Login to PCI ControlScan">
+		<input class="button ssaq_button" type="Submit" value="Login to PCI ControlScan" onclick="WriteCookie();">
 	</div>
 </form>
-<?php
-if (isset($_POST['remember_me'])){
-	$uservalue = $_GET["password"];
-	setcookie('userid', $uservalue, time()+3600);
-}
-?>
