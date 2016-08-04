936a51e052cda356bff3d61ecb408f4e3e60b08f
(retsu8)Wiliam Paddock <wjp@frontline.local>
2016-08-04 17:07:05 -0600

    fixing out
    fixing pciscan

--- pciControlSCan.php
+++ pciControlSCan.php
@@ -1,21 +1,7 @@
-<script type = "text/javascript">
-         <!--
-            function WriteCookie(){
-  						var expiration_date = new Date();
-              var expires = 'expires='+expiration_date.setMonth(expiration_date.getMonth()).toGMTString();
-            	if(document.getElementById('remember_me').checked){
-  							document.cookie = 'userid='+document.admlogin.password.value+';'+expires;
-  						}
-/*              else{
-                expires = 'expires='+expiration_date.setMonth(expiration_date.getMonth()-1).toGMTString();
-                document.cookie = 'userid=;'+expires;
-              }*/
-					  }
-         //-->
-</script>
 <?php
 $nid= arg(1);
 $node = node_load($nid);
+//dsm($_COOKIE);
 ?>
 
 
@@ -23,19 +9,32 @@ $node = node_load($nid);
 	<input type="hidden" value="process" name="a">
 	<input type="hidden" value="overview" name="page_requested">
 	<input id="js" type="hidden" value="1" name="js">
-	<label>Username:</label><input class="ssaq_form" type="text" size="42"  style="background-color: lightgray; width: 180px;" readonly="" name="username" value = "<?php echo $node->field_pci_controlscan_username['und'][0]['value'];   ?>">
+	<label>Username:</label>
+  <input class="ssaq_form" type="text" size="42"  style="background-color: lightgray; width: 180px;" readonly="" name="username" value = "<?php echo $node->field_pci_controlscan_username['und'][0]['value'];   ?>">
 	<br />
-<?php
-if(!isset($_COOKIE["userid"])) {
-    echo '<label>Password:</label>';
-		?><input class="ssaq_form" type="password" size="42" name="password" style = "width: 180px;" >
-<?php } else {
-	echo '<label>Password:</label>'; ?>
- <input class="ssaq_form" type="password" size="42" name="password" style = "width: 180px;" value = "<?php echo $_COOKIE["userid"]?>">	<?php
-}
-?><br>
-		<label class="option" for="remember_me">Remember me: </label><input class="ssaq_form" type="checkbox" name="remember_me" value="true" checked/>
+  <label>Password:</label>
+  <input class="ssaq_form" type="password" size="42" id="pass" name="password" style = "width: 180px;" value = "<?php echo $_COOKIE["userid"]?>">
+  <br>
+		<label class="option" for="remember_me">Remember me: </label>
+    <input class="ssaq_form" id="reME" type="checkbox" name="remember_me" value="true" checked>
 	<div align="left">
-		<input class="button ssaq_button" type="Submit" value="Login to PCI ControlScan" onclick="WriteCookie();">
+		<input class="button ssaq_button" type="Submit" value="Login to PCI ControlScan" onclick="WriteCookie()">
 	</div>
 </form>
+
+<script type = "text/javascript">
+  function WriteCookie (){
+      //document.write("Hello i set a cookie for you.");
+  			var expiration_date = new Date();
+      	if(document.getElementById("reME").value == true){
+            //document.write("This is were i saved it");
+            var expires = "; expires="+expiration_date;
+            document.write(document.getElementById("pass").value);
+            document.cookie = "userid="+document.getElementById("pass").value; //saving the cookie here for Password
+  		 }else{
+           //document.write("Forgot to save it...");
+           var expires = "; expires="+expiration_date;
+           document.cookie = "userid=";
+        }
+	  }
+</script>
